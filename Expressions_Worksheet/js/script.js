501b5723ef6d34e85eb05927f728198709e9b92f
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-10 23:22:12 +0900

    part one

---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,10 @@
+// Bethany Prevatt, October 10, 2013, Expressions Worksheet
+
+//Dog Years
+var dogAge = 5;
+var age = 5 * dogAge;
+
+var result = 5 * 7;
+console.log(age);
+
+//Slice of Pie Part 1
