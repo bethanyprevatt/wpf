6921173444c193c409b64f50d4b2ecf29ced491c
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-10 22:30:27 +0900

    operations

--- associatingJS/js/script.js
+++ associatingJS/js/script.js
@@ -1,10 +1,17 @@
-//Arrays
+//Order of Operations PEMDAS
 
-var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
 
-muppetNames[1] = "Rizzo";
-muppetNames[2] = "Fozzy";
+//the average is the sum of items divided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
 
-var num = 2;
 
-console.log(muppetNames[num]);
+//console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width *2;
+console.log(perimeter);
