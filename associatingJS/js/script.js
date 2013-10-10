bb5f21615aa1b6212cf18d45ddf0bbd4206f638a
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-10 22:58:52 +0900

    casting

--- associatingJS/js/script.js
+++ associatingJS/js/script.js
@@ -1,13 +1,8 @@
-//strings
+//casting
 
-//var firstName = "Bethany";
-//var lastName = "Prevatt";
-//var fullName = firstName + " " + lastName;
-
-//console.log(fullName);
-
-var a = 6;
-var b = 7;
-
-var result = a + b;
-console.log(result);
+var areCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+//(407) 203-4529
+var phoneNo = "(" + String(areCode) + ")" + String(firstPart) + "-" + String(secPart);
+console.log(phoneNo);
