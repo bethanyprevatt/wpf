eeec3622f9035422f46e5d2a2a1e59a4ef818d27
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-10 23:05:10 +0900

    assignmentoperators

--- associatingJS/js/script.js
+++ associatingJS/js/script.js
@@ -1,8 +1,5 @@
-//casting
+//Assignment Operators
 
-var areCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-//(407) 203-4529
-var phoneNo = "(" + String(areCode) + ")" + String(firstPart) + "-" + String(secPart);
-console.log(phoneNo);
+var a = 3;
+a ++; //same as typing a = a + 1 //a += 1
+console.log(a);
