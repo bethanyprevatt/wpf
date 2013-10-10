de1c5c6d66b7efa508a5679d49056d2c2cf30107
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-10 23:30:36 +0900

    pizzapartone

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -7,4 +7,16 @@ var age = 5 * dogAge;
 var result = 5 * 7;
 console.log(age);
 
-//Slice of Pie Part 1
+//Slice of Pie Part I
+var slices = 8;
+var pizzaOrder = 2;
+var people = 3;
+
+var result = slices * pizzaOrder/people;
+console.log(result);
+
+//Slice of Pie Part II
+var slices = 8;
+var pizzaOrder = 2;
+var people = 3;
+
