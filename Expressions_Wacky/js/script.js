5469a22ac1917d7013b5f07fc929150e4f1d9d43
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-11 00:39:11 +0900

    newgithubs

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,44 @@
+// Bethany Prevatt, October 10, 2013, Expressions Worksheet
+
+//Dog Years
+var dogAge = 5;
+var age = 5 * dogAge;
+
+var result = 5 * 7;
+console.log(age);
+
+//Slice of Pie Part I
+var slices = 8;
+var pizzaOrder = 4;
+var people = 10;
+
+var result = slices * pizzaOrder/people;
+console.log(result);
+
+//Slice of Pie Part II
+var slices = 8;
+var pizzaOrder = 4;
+var people = 10;
+
+var result = slices * pizzaOrder/people;
+console.log("Sparky got " + result + " slices of pizza");
+
+//Average Shopping Bill
+var groceryBills = [125, 310, 122, 205, 320];
+
+var total = groceryBills[0] + groceryBills[1] + groceryBills[2] + groceryBills[3] + groceryBills[4];
+var week = (groceryBills[0] + groceryBills[1] + groceryBills[2] + groceryBills[3] + groceryBills[4])/5;
+
+console.log("You have spent a total of $" + total + " over 5 weeks. That is an average of $" + week + " per week");
+
+//Discounts
+var dresser = "dresser";
+var originalPrice = 450;
+var discount = .20;
+var salesTax = .06;
+
+var withoutTax = orginalPrice * discount;
+var withTax = originalPrice * discount * salesTax;
+
+console.log("Your" + dresser + " was originally $" + orignalPrice + " but after a" + discount + "%, it is now $" + withoutTax + "without tax, and $" + withTax);
+//not sure how to complete this one?
