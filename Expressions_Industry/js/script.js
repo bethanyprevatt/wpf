1ed811e9c2d0be01840b3fc6d8683d4619424298
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-11 00:44:26 +0900

    industry middle of code

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,9 +1,8 @@
 // Bethany Prevatt, October 10, 2013, Expressions Industry
 
-//Slice of Pie Part I
-var slices = 8;
-var pizzaOrder = 4;
-var people = 10;
+var projects = 12;
+var employees = 4;
+var  = 10;
 
 var result = slices * pizzaOrder/people;
 console.log(result);
