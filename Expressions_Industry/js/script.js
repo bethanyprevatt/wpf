7320b5aa2796a9ee0b61583a2b0237c9b563ba7c
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-11 00:55:44 +0900

    completeindustry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,7 +2,8 @@
 
 var projects = 12;
 var employees = 4;
-var  = 10;
+var hours = 45;
 
-var result = slices * pizzaOrder/people;
-console.log(result);
+var projectsPerEmployee = projects/employees;
+var hoursForProject = hours/projects;
+console.log("Each employee will need to complete " + projectsPerEmployee + " projects and they have " + hoursForProject + " hours to complete each project");
