38471047ab155c50eadd96efed79673f88a271ee
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-17 18:49:02 +0900

    exercises four

--- Conditional Exercises/js/script.js
+++ Conditional Exercises/js/script.js
@@ -17,4 +17,17 @@ if(kidHeight > minHeight){
 	console.log("Sorry kid, you've got some growing to do first!")
 }
 
+//conditional logic - logical operators
+
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+//if the price of the phone is less our budget and if our paycheck is over 300
+if(iPhonePrice < budget || wonLottery){
+	console.log("We can buy the phone!");
+}else{
+	console.log("No phone for you.");
+}
+
 
