6277e94f028187221d1c3745f331fcc64cc98cfd
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-17 20:39:36 +0900

    comments

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,5 +5,6 @@
 var rating = 96;
 var requiredRating = 90;
 
+//if the rating will be above given required rating, the website is successful
 (rating > requiredRating) ? console.log("Your website is successful") : console.log("You will need to boost your ratings!");
 
