670d457abdbed8330238dd7e59cbb55c28203202
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-17 18:32:09 +0900

    exerise three

--- Conditional Exercises/js/script.js
+++ Conditional Exercises/js/script.js
@@ -1,15 +1,20 @@
-//conditional logic
+//conditional logic = else if
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
+var parentHeight = 45; //height of the kid with the parent
 
 //if the child is old enough print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
-	// code preformed if condition is true
+if(kidHeight > minHeight){
+	// you can ride
 	console.log("You can ride the coaster!");
+}else if(kidHeight > parentHeight){
+	//you can ride with a parent present
+	console.log("You can ride, but only with a parent present.")
+}else{
+	//sorry you have growing to do
+	console.log("Sorry kid, you've got some growing to do first!")
 }
-
 
 
