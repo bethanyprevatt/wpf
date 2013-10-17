4bc47e182e07391250e06193d1e49e0fec3f1e62
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-17 18:18:36 +0900

    exercises two

--- Conditional Exercises/js/script.js
+++ Conditional Exercises/js/script.js
@@ -1,10 +1,15 @@
 //conditional logic
 
-var oldEnough = true;
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
 
 //if the child is old enough print to the console "you can ride!"
-if(oldEnough){
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
 	// code preformed if condition is true
 	console.log("You can ride the coaster!");
 }
+
+
 
