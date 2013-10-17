e984afa85b8f3563f14773f2a7cc9a4283ff3ef8
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-17 18:08:34 +0900

    exercises

---
+++ conditionallogicscreencast.js
@@ -0,0 +1,10 @@
+//conditional logic
+
+var oldEnough = true;
+
+//if the child is old enough print to the console "you can ride!"
+if(oldEnough){
+	// code preformed if condition is true
+	console.log("You can ride the coaster!");
+}
+
