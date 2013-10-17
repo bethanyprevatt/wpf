629da67a177333de739ae16e1736dde2c44c6f0f
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-17 19:00:37 +0900

    final exercise

--- Conditional Exercises/js/script.js
+++ Conditional Exercises/js/script.js
@@ -30,4 +30,29 @@ if(iPhonePrice < budget || wonLottery){
 	console.log("No phone for you.");
 }
 
+//Ternary Operators
 
+var gpa = 48;
+
+//if the gpa is over the 2.0 score, the student can graduate
+/*if(gpa > 2.0){
+	console.log("You can graduate!");
+}else{
+	console.log("GPA is too low!");
+}
+*/
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+
+var age = 6;
+var book;
+
+//If the child is under 10, they get green eggs and ham, otherwise they get The Time Machine
+/*if(age < 10){
+	book = "Green Eggs and Ham";
+}else{
+	book = "The Time Machine";
+}
+console.log(book);*/
+
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
