c3ec6c376db0d7eca4bab822f46b6a14e873460d
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-24 21:03:31 +0900

    drinkingage

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,6 +1,7 @@
 // Bethany Prevatt, October 24, 2013, Functions Wacky
 
-//
+//drinking age
 
 var age = 20;
 (age > 21) ? console.log("You can drink") : console.log("You are too young to drink!");
+
