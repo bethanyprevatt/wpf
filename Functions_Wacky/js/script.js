271dc668b0a32e17b134aae0b18bf2d3f20db52a
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-24 20:38:24 +0900

    functionsIndustry

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,9 @@
+// Bethany Prevatt, October 10, 2013, Expressions Wacky
+
+var one = prompt("We are multiplying three items. \nPlease enter the first item amount:");
+var two = prompt("Enter second item amount:");
+var three = prompt("Enter third item amount:");
+
+var total = one * two * three;
+
+alert("The average age of the three people are " + total);
