fb21f50cb7aa7c514c7a9f7bdea56fdf98747708
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-24 20:56:51 +0900

    personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,2 +1,10 @@
 // Bethany Prevatt, October 24, 2013, Functions Personal
 
+//Shampoo, conditoner and wash bottle cost
+
+var bottles = function(shampoo, conditioner, wash){
+	var cost = shampoo + conditioner + wash;
+	return cost;
+}
+var a = cost(5, 10, 8);
+console.log("Your total is $" + a);
