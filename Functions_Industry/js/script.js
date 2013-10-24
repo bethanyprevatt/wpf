8cc849c56a615b0d5ca76502125077deb40c7e84
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-24 20:46:44 +0900

    industrycode

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,2 +1,11 @@
 // Bethany Prevatt, October 24, 2013, Functions Industry
 
+//calculate the cost of three computers
+
+var computerCost  = function(cost, amount){
+	var totalCost = cost * amount;
+	return totalCost;
+}
+var a = computerCost(2500, 25);
+console.log("Total cost for the computers will be " + a ".");
+
