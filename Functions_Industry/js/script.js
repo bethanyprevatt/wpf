6348aac24321fb8387b93565d8a46de080d8e491
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-24 20:49:08 +0900

    industryfixes

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,9 +2,9 @@
 
 //calculate the cost of three computers
 
-var computerCost  = function(cost, amount){
-	var totalCost = cost * amount;
-	return totalCost;
+var computerCost = function(cost, amount){
+	var total = cost * amount;
+	return total;
 }
 var a = computerCost(2500, 25);
 console.log("Total cost for the computers will be " + a ".");
