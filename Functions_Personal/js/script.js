ae72785525cfcdaeb676f3a27e5f5fb1ad869a96
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-24 20:38:50 +0900

    functionspersonal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,8 +1,2 @@
-// Bethany Prevatt, October 10, 2013, Expressions Personal
+// Bethany Prevatt, October 24, 2013, Functions Personal
 
-var householdBills = [264, 20, 76, 145];
-
-var total = householdBills[0] + householdBills[1] + householdBills[2] + householdBills[3];
-var week = (householdBills[0] + householdBills[1] + householdBills[2] + householdBills[3])/4;
-
-console.log("Bills are a total of $" + total + " over the past month. That is an average of $" + week + " per week");
