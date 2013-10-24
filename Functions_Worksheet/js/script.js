afd4fc134b36e40f392518c5e972dbf8f124d046
bethanyprevatt <bethany.prevatt@fullsail.edu>
2013-10-24 20:30:19 +0900

    initial

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,2 +1,13 @@
 // Bethany Prevatt, October 24, 2013, Functions Worksheet
 
+//circumference c=3.14*d
+
+var circle = function(diameter, pie){
+	var circumference = diameter * pie;
+	return circumference; 
+}
+var a = circle(25, 3.14);
+console.log(a);
+
+
+//stung
